a550372baaa2266c682219f649df5d4e6bfcb478
thaisealves <alves.thaisesilva@gmail.com>
2022-09-19 11:41:29 -0300

    feat: adding the tests for reading the exam by the teacher

--- tests/getExam.test.ts
+++ tests/getExam.test.ts
@@ -74,7 +74,71 @@ describe("Testing /GET on discipline showing exams", () => {
   });
 });
 
+describe("Testing /GET on teacher to show exams", () => {
+  it("Must return 200 and an array with all the exams by teacher", async () => {
+    const user = newUser();
+    const exam = newExam();
 
+    const loginUser = {
+      email: user.email,
+      password: user.password,
+    };
+    await supertest(app).post("/signup").send(user);
+    const getToken = await supertest(app).post("/signin").send(loginUser);
+
+    await supertest(app)
+      .post("/exam")
+      .send(exam)
+      .set({ Authorization: `Bearer ${getToken.body.token}` });
+
+    const getExams = await supertest(app)
+      .get("/teacher")
+      .send()
+      .set({ Authorization: `Bearer ${getToken.body.token}` });
+
+    console.log(getExams.body);
+    expect(getExams.status).toBe(200);
+    expect(getExams.body).toBeInstanceOf(Array);
+    expect(getExams.body[0].categories.category).not.toBeNull;
+  });
+
+  it("Must return 401 if it doesn't have authorization", async () => {
+    const user = newUser();
+    const exam = newExam();
+
+    const loginUser = {
+      email: user.email,
+      password: user.password,
+    };
+    await supertest(app).post("/signup").send(user);
+    const getToken = await supertest(app).post("/signin").send(loginUser);
+
+    await supertest(app)
+      .post("/exam")
+      .send(exam)
+      .set({ Authorization: `Bearer ${getToken.body.token}` });
+    const getExams = await supertest(app).get("/teacher").send();
+    expect(getExams.status).toBe(401);
+  });
+
+  it("Must not have exams on the body if the db is empty", async () => {
+    const user = newUser();
+
+    const loginUser = {
+      email: user.email,
+      password: user.password,
+    };
+    await supertest(app).post("/signup").send(user);
+    const getToken = await supertest(app).post("/signin").send(loginUser);
+
+    const getExams = await supertest(app)
+      .get("/teacher")
+      .send()
+      .set({ Authorization: `Bearer ${getToken.body.token}` });
+
+    expect(getExams.body[0].categories.category).toBeNull;
+  });
+});
 
 afterAll(async () => {
   await prisma.$disconnect();
